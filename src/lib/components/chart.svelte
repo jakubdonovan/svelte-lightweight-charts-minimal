7d7c2095661897244e039e2b2ddbbc23d560aa8f
Jakubdonovan <jakubdonovan@protonmail.com>
2022-09-08 20:31:08 +0700

    updated

--- src/lib/components/chart.svelte
+++ src/lib/components/chart.svelte
@@ -1,6 +1,5 @@
 <script>
 	import { onMount, onDestroy } from 'svelte';
-	import { widget } from '$lib/charting_library';
 
 	function getLanguageFromURL() {
 		const regex = new RegExp('[\\?&]lang=([^&#]*)');
@@ -11,7 +10,7 @@
 	export let symbol = 'AAPL';
 	export let interval = 'D';
 	export let datafeedUrl = 'https://demo_feed.tradingview.com';
-	export let libraryPath = '../../src/lib/charting_library/';
+	export let libraryPath = '/charting_library/';
 	export let chartsStorageUrl = 'https://saveload.tradingview.com';
 	export let chartsStorageApiVersion = '22.032';
 	export let clientId = 'tradingview.com';
@@ -23,38 +22,36 @@
 	let chartContainer;
 	let tvWidget = null;
 
-	onMount(() => {
-		setTimeout(() => {
-			new window.Datafeeds.UDFCompatibleDatafeed(datafeedUrl);
-			const widgetOptions = {
-				symbol,
-				datafeed: new window.Datafeeds.UDFCompatibleDatafeed(datafeedUrl),
-				interval: interval,
-				container: chartContainer,
-				library_path: libraryPath,
-				locale: getLanguageFromURL() || 'en',
-				disabled_features: ['use_localstorage_for_settings'],
-				enabled_feeatures: ['study_templates'],
-				charts_storage_url: chartsStorageUrl,
-				charts_storage_api_version: chartsStorageApiVersion,
-				client_id: clientId,
-				user_id: userId,
-				fullscreen: fullscreen,
-				autosize: autosize,
-				studies_overrides: studiesOverrides,
-				debug: true
-			};
-			tvWidget = new widget(widgetOptions);
+	onMount(async () => {
+		console.log(window.Datafeeds);
 
-			console.log('WINDOW:', window);
-			console.log('MOUNTED:', tvWidget);
-			console.log('CHART CONTEINER:', chartContainer);
-		}, 500);
+		const { widget } = await import('$lib/charting_library');
+
+		new window.Datafeeds.UDFCompatibleDatafeed(datafeedUrl);
+		const widgetOptions = {
+			symbol,
+			datafeed: new window.Datafeeds.UDFCompatibleDatafeed(datafeedUrl),
+			interval: interval,
+			container: chartContainer,
+			library_path: libraryPath,
+			locale: getLanguageFromURL() || 'en',
+			disabled_features: ['use_localstorage_for_settings'],
+			enabled_feeatures: ['study_templates'],
+			charts_storage_url: chartsStorageUrl,
+			charts_storage_api_version: chartsStorageApiVersion,
+			client_id: clientId,
+			user_id: userId,
+			fullscreen: fullscreen,
+			autosize: autosize,
+			studies_overrides: studiesOverrides,
+			debug: true
+		};
+		tvWidget = new widget(widgetOptions);
 	});
 </script>
 
 <svelte:head>
-	<script src="../../src/lib/datafeeds/udf/dist/bundle.js"></script>
+	<script src="/datafeeds/udf/dist/bundle.js"></script>
 </svelte:head>
 
 <div class="TVChartContainer h-screen" bind:this={chartContainer} />
